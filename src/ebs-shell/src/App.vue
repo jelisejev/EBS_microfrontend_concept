d2c644d4f746efb7e3879619fba1221c159810be
pavel.yeliseev <jelisejev@users.noreply.github.com>
2018-12-20 15:54:19 +0200

    resize iframes according to the content

--- src/ebs-shell/src/App.vue
+++ src/ebs-shell/src/App.vue
@@ -64,11 +64,12 @@ export default {
     color: #2c3e50;
     display: flex;
     flex-direction: column;
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
+    height: 100vh;
+    /*position: absolute;*/
+    /*top: 0;*/
+    /*left: 0;*/
+    /*right: 0;*/
+    /*bottom: 0;*/
   }
 
   main {
--- src/ebs-shell/src/micro-frontends/EbsApp.vue
+++ src/ebs-shell/src/micro-frontends/EbsApp.vue
@@ -4,6 +4,8 @@
 
 <script>
 import {connect} from "./EventBus";
+import iFrameResize from 'iframe-resizer/js/iframeResizer'
+
 
 let unsubscribe;
 
@@ -14,6 +16,14 @@ export default {
   },
   mounted() {
     unsubscribe = connect(this.$refs.iframe.contentWindow);
+
+    iFrameResize({
+      log: true,
+      autoResize: true,
+      heightCalculationMethod: 'bodyScroll',
+      // sizeHeight: true,
+    }, this.$refs.iframe)
+
   },
   beforeDestroy() {
     unsubscribe();
